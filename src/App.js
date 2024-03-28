285412e09ef88b61f197ad4361626288189a500b
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-03-28 18:33:28 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -7,36 +7,33 @@ import Mycart from "./routes/Mycart";
 import Contact from "./routes/Contact";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BooksPage from "./pages/bookspage/BooksPage";
-import BookDetailsPage from "./pages/bookspage/bookdetailspage/BookDetails";
+// import BookDetailsPage from './pages/bookspage/bookdetailspage/BookDetails';
 import SingleBook from "./pages/bookspage/SingleBook";
-
 import Register from "./pages/RegisterPage/Register";
 import Login from "./pages/LoginPage/Login";
 
 export default function App() {
   return (
-    <>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/service" element={<Mycart />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/books" element={<BooksPage />} />
-            <Route path="/book-details/:id" element={<BookDetailsPage />} />
-            <Route
-              path="/book/:id"
-              element={<SingleBook />}
-              loader={({ params }) =>
-                fetch(`http://localhost:5000/book/${params.id}`)
-              }
-            />
-            <Route path="/Register" element={<Register />} />
-            <Route path="/Login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Mycart />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/books" element={<BooksPage />} />
+          {/* <Route path="/book-details/:id" element={<BookDetailsPage/>} /> */}
+          <Route
+            path="/book/:id"
+            element={<SingleBook />}
+            loader={({ params }) =>
+              fetch(`http://localhost:5000/book/${params.id}`)
+            }
+          />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
