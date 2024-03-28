a88d58b596fc19ddbcfd8e583f395d365abb034b
Devraj Mudgal <devrajmudgal17@gmail.com>
2024-03-28 18:46:27 +0530

    Completed Register and Login Page

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import "./Login.style.css";
+import aboutimg from "../../assets/aboutimg.jpg";
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+
+
 
 const Login = () => {
   return (
-    <div>
-        <h1>Login Page</h1>
+
+    <React.Fragment>
+    <Navbar />
+    <div className="Login-Container">
+        <div className='Login-Image-Container'>
+            <img src={aboutimg} alt='Authentication-Background'/>
+        </div>
+        <div className='Login-Content-Container'>
+            <div>
+                <h2>Login</h2>
+                <p>Login With Email and Password</p>
+
+                <form  className='Content-Wrapper'>
+
+                    
+                    <div className='login-input-form-container'>
+                        <label className='login-input'>Email :</label>
+                        <input type='Email' placeholder='Enter Your Email' className='login-input'/>
+                    </div>
+
+                    
+                    <div className='login-input-form-container'>
+
+                        <label className='login-input'>Enter Password :</label>
+                        <input type='Password' className='login-input'    placeholder='Enter your Password'/>
+                    </div>
+                    <div>
+                        <input type='submit' className='login-button' value="Login"/>
+                    </div>
+                    <p>Are you a New User? </p><Link to="/Register">Register</Link>
+                </form>
+            </div>
+        </div>
+        
     </div>
+
+    </React.Fragment>
   )
 }
 
--- src/pages/RegisterPage/Register.js
+++ src/pages/RegisterPage/Register.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
 import "./Register.style.css";
+import contactimg from "../../assets/contactimg.jpg";
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
 
 const Register = () => {
   return (
-    <div>
-        <h1>Register Page</h1>
+    <React.Fragment>
+    <Navbar />
+    <div className="Register-Container">
+        <div className='Register-Image-Container'>
+            <img src={contactimg} alt='Authentication-Background'/>
+        </div>
+        <div className='Register-Content-Container'>
+            <div>
+                <h2>Register New User</h2>
+                <p>Register With Email and Password</p>
+
+                <form  className='Content-Wrapper'>
+
+                    <div className='Register-input-form-container'>
+                            <label className='Register-input'>Name :</label>
+                            <input type='text' placeholder='Enter Your Name' className='Register-input'/>
+                    </div>
+                    
+                    <div className='Register-input-form-container'>
+                        <label className='Register-input'>Email :</label>
+                        <input type='Email' placeholder='Enter Your Email' className='Register-input'/>
+                    </div>
+
+                    
+                    <div className='Register-input-form-container'>
+
+                        <label className='Register-input'>Enter Password :</label>
+                        <input type='Password' className='Register-input'    placeholder='Enter your Password'/>
+                    </div>
+                    <div>
+                        <input type='submit' className='Register-button' value="Register"/>
+                    </div>
+
+                    <p>Already Existing User?</p><Link to="/Login">Login</Link>
+
+                    
+
+                </form>
+            </div>
+        </div>
+        
     </div>
+    </React.Fragment>
   )
 }
 
