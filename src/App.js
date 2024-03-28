9e1e83b68eab7654a874e0308367d9f513a20fa8
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-03-28 17:51:37 +0530

    adding some more requirements

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from 'react'
-import "./styles.css"
-
-import Home from './routes/Home';
-import About from './routes/About';
-import Mycart from './routes/Mycart';
-import Contact from './routes/Contact';
-import {Route, Routes } from 'react-router-dom';
-import BooksPage from './pages/bookspage/BooksPage';
-import BookDetailsPage from './pages/bookspage/bookdetailspage/BookDetails';
-
-
+import React from "react";
+import "./styles.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Mycart from "./routes/Mycart";
+import Contact from "./routes/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BooksPage from "./pages/bookspage/BooksPage";
+// import BookDetailsPage from "./pages/bookspage/bookdetailspage/BookDetails";
+import SingleBook from "./pages/bookspage/SingleBook";
 export default function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/service" element={<Mycart/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/books" element={<BooksPage/>} />
-        <Route path="/book-details/:id" element={<BookDetailsPage/>} />
-        
-      </Routes>
-      
-      {/* <Home /> */}
-      
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/service" element={<Mycart />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/books" element={<BooksPage />} />
+            {/* <Route path="/book-details/:id" element={<BookDetailsPage />} /> */}
+            <Route
+              path="/book/:id"
+              element={<SingleBook />}
+              loader={({ params }) =>
+                fetch(`http://localhost:5000/book/${params.id}`)
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+        {/* <Home /> */}
+      </div>
+    </>
   );
 }
---
+++ src/pages/bookspage/SingleBook.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const SingleBook = () => {
+  const { _id, title } = useLoaderData();
+  return (
+    <div className="mt-28 px-4 lg:px-24">
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+export default SingleBook;
